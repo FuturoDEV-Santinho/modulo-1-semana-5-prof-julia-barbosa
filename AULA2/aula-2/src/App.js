bb75fa1990be858eb5baa42c48f210b8720c755b
julia <juiolanda@hotmail.com>
2023-01-25 10:46:14 -0300

    codigos das aulas 1 e 2

---
+++ AULA2/aula-2/src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import PrimeiroComponente from './components/PrimeiroComponente';
+
+function App() {
+  return (
+    <div>
+      <PrimeiroComponente></PrimeiroComponente>
+    </div>
+  );
+}
+
+export default App;
---
+++ AULA2/aula-2/src/components/PrimeiroComponente.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const PrimeiroComponente = () => {
+  return (
+    <div>
+        Primeiro Componente com a turma futuro dev
+    </div>
+  )
+}
+
+export default PrimeiroComponente;
